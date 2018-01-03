3a0238d3e5ae2c7d67e7983600a31a7457014515
Nick Quan <nickkquan@gmail.com>
2018-01-03 14:21:07 -0800

    comments added to DOM creation function.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 $(document).ready(initializeApp);
 
-function createGameBoard(row,column) {
-    var rows = row || 6;
-    var column = column || 7;
+function createGameBoard(row,column) { // Function that creates the game board dynamically. Takes in number of rows and columns.
+    var rows = row || 6; // Number of rows will be either what is dictated or defaults to 6.
+    var column = column || 7; // Number of columns will be either what is dictated or defaults to 7.
 
-    for (var columnIndex = 0; columnIndex<column; columnIndex++) {
+    for (var columnIndex = 0; columnIndex<column; columnIndex++) { // For loop that creates columns.
         var columnElement = $('<div>')
             .addClass('column')
             .attr('id','column'+columnIndex);
-        for (var heightIndex = 0; heightIndex<rows; heightIndex++) {
+        for (var heightIndex = 0; heightIndex<rows; heightIndex++) { // For loop that creates tiles in columns.
             var tileElement = $('<div>')
                 .addClass('tile')
                 .attr('id','r' + heightIndex + 'c' + columnIndex);
@@ -16,7 +16,6 @@ function createGameBoard(row,column) {
         }
         columnElement.appendTo($('#gameBoard'));
     }
-
 }
 
 function clickHandler(){
