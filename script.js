86ab066c278abd593ddf57398f63dd9dbe958de4
Nick Quan <nickkquan@gmail.com>
2018-01-03 15:15:18 -0800

    added function that determines which player goes first. added additioinal comments to code for legibility

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 $(document).ready(initializeApp);
 
-function createGameBoard(row,column) {
-    var rows = row || 6;
-    var column = column || 7;
+function createGameBoard(row,column) { // Function that creates game board.
+    var rows = row || 6; // Declared variable takes in number of rows or defaults to six.
+    var column = column || 7; // Declared variable takes in number of columns or defaults to seven.
 
-    for (var columnIndex = 0; columnIndex<column; columnIndex++) {
+    for (var columnIndex = 0; columnIndex<column; columnIndex++) { // For loop that creates columns.
         var columnElement = $('<div>')
             .addClass('column')
             .attr('id','column'+columnIndex);
-        for (var heightIndex = 0; heightIndex<rows; heightIndex++) {
+        for (var heightIndex = 0; heightIndex<rows; heightIndex++) { // For loop that creates tiles.
             var tileElement = $('<div>')
                 .addClass('tile')
                 .attr('id','r' + heightIndex + 'c' + columnIndex);
@@ -16,7 +16,6 @@ function createGameBoard(row,column) {
         }
         columnElement.appendTo($('#gameBoard'));
     }
-
 }
 
 function clickHandler(){
@@ -26,7 +25,15 @@ function clickHandler(){
     )
 }
 
+function getFirstMove(){ // Determines which player gets to place token down first.
+    var random = Math.ceil(Math.random() * playerArr.length); // Creates a random number based on the length of array.
+    while (random !== playerArr[0].playerNumber) { // As long as player at 0 does not equal random number
+        cyclePlayers(); // continue to cycle array.
+    }
+}
+
 function initializeApp(){
     createGameBoard();
     clickHandler();
+    getFirstMove();
 }
