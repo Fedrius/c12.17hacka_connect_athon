1616a11e1e04d5e4458a6e30fa44c95cbdc8d1bf
Paul <paulmlee7@gmail.com>
2018-01-03 14:54:37 -0800

    vertical and horizontal checks

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 //check drop position and store token functions
 
 var gameboardArr = null;
@@ -7,16 +8,52 @@ var dropPosition = [];
 //colIndex is the selected column index
 //last index of x is the most bottom position in the column
 
-function checkDropPosition(id){
-    var colIndex = parseInt(/[0-9]/g.match[id]);
+function checkDroppedPosition(id){
+    var colIndex = parseInt(/[0-9]/g.match[id].join(''));
     dropPosition.push(colIndex, gameboardArr[colIndex].lastIndexOf('x'));
 }
 
 //need to declare current player token..
-//gameboardArr[column, position]
+//gameboardArr[column][row position]
 
-function storeToken(posArray){
-    currentPlayerToken = gameboardArr[posArray[0]][posArray[1]];
+function storeToken(DropPosArray){
+    gameboardArr[DropPosArray[0]][DropPosArray[1]] = playerArr[0].tokenColor;
     updateDisplay();
 }
 
+function checkVerticalWin(colIndex){
+    var theTokenColumn = gameboardArr[colIndex];
+
+    var match = 0;
+    for(var columnIndex = 0; columnIndex < theTokenColumn.length - 1; columnIndex++){
+        if(theTokenColumn[columnIndex] === theTokenColumn[columnIndex + 1]){
+            match++
+        } else {
+            match = 0;
+        }
+    }
+
+    if(match > 3){
+       console.log('winnnner')//player wins
+    }
+}
+
+//
+
+function checkHorizontalWin(rowPosIndex){
+
+    var match = 0;
+    for(var columnIndex = 0; columnIndex < gameboardArr.length - 1; columnIndex++){
+        if(gameboardArr[columnIndex][rowPosIndex] === gameboardArr[columnIndex + 1][rowPosIndex]){
+            match++
+        } else {
+            match = 0;
+        }
+    }
+
+    if(match > 3){
+        console.log('winner');
+    }
+}
+
+//need to go through the gameboardArr
