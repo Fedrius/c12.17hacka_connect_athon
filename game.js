05fe9d4ca5ccd0afa95b027a39134ca4d24b6f1d
jasoncp <24683452+jasoncp@users.noreply.github.com>
2018-01-04 13:16:28 -0800

    called randomizeFirstPlayer after game is ready

--- game.js
+++ game.js
@@ -89,6 +89,7 @@ function hideIntro(){
     $('.main-splash-container').hide();
     $('#gameBoard').css('display', 'flex');
     $('.background').css('opacity', 0.2);
+    randomizeFirstMove();
 }
 
 
@@ -153,7 +154,7 @@ function addClickHandlers(){
 function checkMove() {
 
 }
-function getFirstMove(){ // Determines which player gets to place token down first.
+function randomizeFirstMove(){ // Determines which player gets to place token down first.
     var random = Math.ceil(Math.random() * playerArr.length); // Creates a random number based on the length of array.
     while (random !== playerArr[0].playerNumber) { // As long as player at 0 does not equal random number
         cyclePlayers(playerArr); // continue to cycle array.
